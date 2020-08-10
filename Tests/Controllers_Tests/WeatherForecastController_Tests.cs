77c0f19a144631f4fa29384f499d2dcec4af6e1c
Jeremy Wells <jsheridanwells@gmail.com>
2020-08-10 13:00:20 -0500

    added exception handling and tests

--- Tests/Controllers_Tests/WeatherForecastController_Tests.cs
+++ Tests/Controllers_Tests/WeatherForecastController_Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
@@ -25,6 +26,49 @@ namespace WeatherWalkingSkeleton.Tests.Controllers_Tests
 
             Assert.IsType<List<WeatherForecast>>(result.Value);
             Assert.Equal(200, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task Returns_400_Result_When_Missing_Location()
+        {
+            var opts = OptionsBuilder.OpenWeatherConfig();
+            var clientFactory = ClientBuilder.OpenWeatherClientFactory(OpenWeatherResponses.NotFoundResponse);
+            var service = new OpenWeatherService(opts, clientFactory);
+            var sut = new WeatherForecastController(new NullLogger<WeatherForecastController>(), service);
+
+            var result = await sut.Get(String.Empty) as ObjectResult;
+            
+            Assert.Equal(400, result.StatusCode);
+        }
+        
+        [Fact]
+        public async Task Returns_BadRequestResult_When_Location_Not_Found()
+        {
+            var opts = OptionsBuilder.OpenWeatherConfig();
+            var clientFactory = ClientBuilder.OpenWeatherClientFactory(OpenWeatherResponses.NotFoundResponse,
+                HttpStatusCode.NotFound);
+            var service = new OpenWeatherService(opts, clientFactory);
+            var sut = new WeatherForecastController(new NullLogger<WeatherForecastController>(), service);
+            
+            var result = await sut.Get("Westworld") as ObjectResult;
+            
+            Assert.Contains("not found", result.Value.ToString());
+            Assert.Equal(400, result.StatusCode);
+        }
+        
+        [Fact]
+        public async Task Returns_500_When_Api_Returns_Error()
+        {
+            var opts = OptionsBuilder.OpenWeatherConfig();
+            var clientFactory = ClientBuilder.OpenWeatherClientFactory(OpenWeatherResponses.UnauthorizedResponse,
+                HttpStatusCode.Unauthorized);
+            var service = new OpenWeatherService(opts, clientFactory);
+            var sut = new WeatherForecastController(new NullLogger<WeatherForecastController>(), service);
+            
+            var result = await sut.Get("Rio de Janeiro") as ObjectResult;
+            
+            Assert.Contains("Error response from OpenWeatherApi: Unauthorized", result.Value.ToString());
+            Assert.Equal(500, result.StatusCode); 
         }
     }
 }
--- Tests/Services_Tests/OpenWeatherService_Tests.cs
+++ Tests/Services_Tests/OpenWeatherService_Tests.cs
@@ -35,5 +35,41 @@ namespace WeatherWalkingSkeleton.Services
             Assert.Equal(new DateTime(1594155600), result[0].Date);
             Assert.Equal((decimal)32.93, result[0].Temp);
         }
+        
+        [Fact]
+        public async Task Returns_OpenWeatherException_When_Called_With_Bad_Argument()
+        {
+            var opts = OptionsBuilder.OpenWeatherConfig();
+            var clientFactory = ClientBuilder.OpenWeatherClientFactory(OpenWeatherResponses.NotFoundResponse,
+                HttpStatusCode.NotFound);
+            var sut = new OpenWeatherService(opts, clientFactory);
+ 
+            var result = await Assert.ThrowsAsync<OpenWeatherException>(() => sut.GetFiveDayForecastAsync("Westeros"));
+            Assert.Equal(404, (int)result.StatusCode);
+        }
+        
+        [Fact]
+        public async Task Returns_OpenWeatherException_When_Unauthorized()
+        {
+            var opts = OptionsBuilder.OpenWeatherConfig();
+            var clientFactory = ClientBuilder.OpenWeatherClientFactory(OpenWeatherResponses.UnauthorizedResponse,
+                HttpStatusCode.Unauthorized);
+            var sut = new OpenWeatherService(opts, clientFactory);
+
+            var result = await Assert.ThrowsAsync<OpenWeatherException>(() => sut.GetFiveDayForecastAsync("Chicago"));
+            Assert.Equal(401, (int)result.StatusCode);
+        }
+        
+        [Fact]
+        public async Task Returns_OpenWeatherException_On_OpenWeatherInternalError()
+        {
+            var opts = OptionsBuilder.OpenWeatherConfig();
+            var clientFactory = ClientBuilder.OpenWeatherClientFactory(OpenWeatherResponses.InternalErrorResponse,
+                HttpStatusCode.InternalServerError);
+            var sut = new OpenWeatherService(opts, clientFactory);
+ 
+            var result = await Assert.ThrowsAsync<OpenWeatherException>(() => sut.GetFiveDayForecastAsync("New York"));
+            Assert.Equal(500, (int)result.StatusCode);
+        }
     }
 }
